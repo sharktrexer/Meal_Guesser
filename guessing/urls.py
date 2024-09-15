d95326aadf1050109518d25c62a0583476325323
sharktrexer <sharktrexer@gmail.com>
2024-09-14 18:39:39 -0700

    random meal image now dislays at /rand_meal

--- guessing/urls.py
+++ guessing/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("name_test/<name>", views.name_test, name="name_test"),
+    path("rand_meal", views.rand_meal, name="rand_meal")
 ]
--- guessing/views.py
+++ guessing/views.py
@@ -1,6 +1,8 @@
-import re
 from django.shortcuts import render
 from django.http import HttpResponse
+import json, urllib.request
+
+
 
 # Views
 
@@ -14,5 +16,25 @@ def name_test(request, name):
         'guessing/index.html',
         {
             'name': name
+        }
+    )
+    
+def rand_meal(request):
+    
+    # Get random picture
+    mealAPI = "https://www.themealdb.com/api/json/v1/1/random.php"
+    
+    with urllib.request.urlopen(mealAPI) as url:
+        api_data = json.load(url)
+        
+    meal_img = api_data['meals'][0]['strMealThumb']
+    meal_name = api_data['meals'][0]['strMeal']
+    print(meal_img)
+    return render(
+        request,
+        'guessing/meal.html',
+        {
+            'meal_img': meal_img,
+            'meal_name': meal_name
         }
     )
